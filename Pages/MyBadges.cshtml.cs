01fd55cbb3bcc191140d1dcea8c6391ea3c0a39a
Jason Young <jason@ytechie.com>
2022-01-25 16:10:32 -0600

    Prototype badge page

--- Pages/MyBadges.cshtml.cs
+++ Pages/MyBadges.cshtml.cs
@@ -5,8 +5,28 @@ namespace BadgeMeUp.Pages
 {
     public class MyBadgesModel : PageModel
     {
-        public void OnGet()
+        public List<Badge> Badges;
+        public string? Alias;
+
+        public MyBadgesModel()
         {
+            Badges = new List<Badge>();
+            Badges.Add(new Badge
+            {
+                Name = "Customer Email",
+                Criteria = "Friendly<br />Clear recap with next steps",
+                BadgeOwnerAlias = "lalovi",
+                BadgeType = BadgeType.SoftSkills,
+                Description = "Awarded when you master the art of the follow-up email"
+            });
+        }
+
+        public void OnGet(string? alias)
+        {
+            if (alias != null)
+                this.Alias = alias;
+            else
+                this.Alias = "jayoung";
         }
     }
 }
