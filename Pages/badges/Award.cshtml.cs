77bb135dd90d655f037a0cc94eb02364698f9230
Jason Young <jason@ytechie.com>
2022-01-27 21:48:50 -0600

    Edit badges from "MyBadges". Stubbed award assignment page.

---
+++ Pages/badges/Award.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BadgeMeUp.Pages.badges
+{
+    public class AwardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
