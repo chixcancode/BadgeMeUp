95dda22d5bb73d8bcf4db63cf0cf1983f05ab92a
Jason Young <jason@ytechie.com>
2022-02-01 12:31:44 -0600

    Added claims list to whoami

--- Pages/WhoAmI.cshtml.cs
+++ Pages/WhoAmI.cshtml.cs
@@ -8,12 +8,17 @@ namespace BadgeMeUp.Pages
     public class WhoAmIModel : PageModel
     {
         public string Name  { get; set; }  
+        public string Claims { get; set; }
 
         public void OnGet()
         {
             if(ClaimsPrincipal.Current != null)
             {
                 Name = ClaimsPrincipal.Current.Identity.Name;
+
+                string claims = "";
+                ClaimsPrincipal.Current.Claims.ToList().ForEach(x => claims += x.Value);
+                Claims = claims;
             }
         }
     }
