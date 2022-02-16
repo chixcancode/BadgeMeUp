cfd89e611c930372ee8e27706f3f4581f3786438
Jason Young <jason@ytechie.com>
2022-02-16 09:49:35 -0600

    Trying a share page to replace the complex award page

---
+++ Pages/badges/Share.cshtml.cs
@@ -0,0 +1,38 @@
+using BadgeMeUp.Db;
+using BadgeMeUp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BadgeMeUp.Pages.Badges
+{
+    public class ShareModel : PageModel
+    {
+        private readonly BadgeDb _badgeDb;
+        private readonly ICurrentUserInfo _userInfo;
+
+        public Badge BadgeToAward { get; set; }
+        public string ShareUrl { get; set; }
+
+        public ShareModel(ICurrentUserInfo userInfo, BadgeDb badgeDb)
+        {
+            _userInfo = userInfo;
+            _badgeDb = badgeDb;
+        }
+
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            BadgeToAward = await _badgeDb.GetBadge(id.Value);
+            var me = _userInfo.GetPrincipalId();
+
+            ShareUrl = AwardModel.CreateShareUrl(id.Value, me);
+
+            return Page();
+        }
+    }
+}
