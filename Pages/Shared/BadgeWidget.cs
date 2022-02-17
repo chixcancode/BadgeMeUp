9315c16977c305be9fa54c7dc6d4f2447b2d63e6
Jason Young <jason@ytechie.com>
2022-02-17 08:19:15 -0600

    Moved the badge display into a reusable widget

---
+++ Pages/Shared/BadgeWidget.cs
@@ -0,0 +1,16 @@
+﻿using BadgeMeUp.Models;
+
+namespace BadgeMeUp.Pages.Shared
+{
+    public class BadgeWidget
+    {
+        public Badge Badge { get; set; }
+        public bool ShowLinks { get; set; } = true;
+
+        public BadgeWidget(Badge badge, bool showLinks = true)
+        {
+            Badge = badge;
+            ShowLinks = showLinks;
+        }
+    }
+}
