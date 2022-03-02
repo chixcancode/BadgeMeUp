1378c14aa30fdee613fdf5c71b8c7cd2e8852e92
LaBrina Loving <labrina.loving@gmail.com>
2022-03-02 12:44:01 -0500

    Adding friendly description to badge

--- Pages/Shared/BadgeWidget.cs
+++ Pages/Shared/BadgeWidget.cs
@@ -13,4 +13,13 @@ public class BadgeWidget
     public Badge Badge { get; set; }
 
     public bool ShowLinks { get; set; } = true;
+
+    public string FriendlyDescription
+    { 
+        get
+        {
+            return Badge.Description.Length > 50 ? Badge.Description.Substring(0, 50) + "..." : Badge.Description;
+        }
+            
+    }
 }
