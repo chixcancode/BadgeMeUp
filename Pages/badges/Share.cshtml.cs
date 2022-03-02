0c045bc3d4327e081fca7573e4233698da5c6599
LaBrina Loving <labrina.loving@gmail.com>
2022-03-02 15:12:33 -0500

    Fixing issue with share

--- Pages/badges/Share.cshtml.cs
+++ Pages/badges/Share.cshtml.cs
@@ -3,38 +3,36 @@ using BadgeMeUp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BadgeMeUp.Pages.Badges;
-
-public class ShareModel : PageModel
+namespace BadgeMeUp.Pages.Badges
 {
-    private readonly BadgeDb _badgeDb;
-
-    private readonly ICurrentUserInfo _userInfo;
-
-    public ShareModel(ICurrentUserInfo userInfo, BadgeDb badgeDb, Badge badgeToAward, string shareUrl)
+    public class ShareModel : PageModel
     {
-        _userInfo = userInfo;
-        _badgeDb = badgeDb;
-        BadgeToAward = badgeToAward;
-        ShareUrl = shareUrl;
-    }
+        private readonly BadgeDb _badgeDb;
+        private readonly ICurrentUserInfo _userInfo;
 
-    public Badge BadgeToAward { get; set; }
+        public Badge BadgeToAward { get; set; }
+        public string ShareUrl { get; set; }
 
-    public string ShareUrl { get; set; }
-
-    public async Task<IActionResult> OnGetAsync(int? id)
-    {
-        if(id == null)
+        public ShareModel(ICurrentUserInfo userInfo, BadgeDb badgeDb)
         {
-            return NotFound();
+            _userInfo = userInfo;
+            _badgeDb = badgeDb;
         }
 
-        BadgeToAward = await _badgeDb.GetBadge(id.Value);
-        var me = _userInfo.GetPrincipalId();
 
-        ShareUrl = ReceiveModel.CreateShareUrl(id.Value, me, null);
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        return Page();
+            BadgeToAward = await _badgeDb.GetBadge(id.Value);
+            var me = _userInfo.GetPrincipalId();
+
+            ShareUrl = ReceiveModel.CreateShareUrl(id.Value, me, null);
+
+            return Page();
+        }
     }
 }
