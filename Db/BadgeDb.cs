bb9e8c9020f151df3b4d1acb1c8b61efccb3c771
Brandon Martinez <me@brandonmartinez.com>
2022-03-03 09:47:55 -0500

    Switching to later execution

--- Db/BadgeDb.cs
+++ Db/BadgeDb.cs
@@ -12,21 +12,6 @@ public class BadgeDb
         _db = dbContext;
     }
 
-    public async Task<Badge?> GetBadge(int badgeId)
-    {
-        return await _db.Badges.Include(x => x.BadgeType).SingleOrDefaultAsync(m => m.Id == badgeId);
-    }
-
-    public async Task<List<AssignedBadge>> GetAssignedBadges(User user)
-    {
-        return await _db.AssignedBadges.Include(x => x.Badge).Where(x => x.User == user).ToListAsync();
-    }
-
-    public async Task<List<AssignedBadge>> GetAssignedBadges(Guid userId)
-    {
-        return await _db.AssignedBadges.Include(x => x.Badge).Where(x => x.User.PrincipalId == userId).ToListAsync();
-    }
-
     public async Task DeleteAssignedBadge(int badgeId)
     {
         var assignment = await GetAssignedBadge(badgeId);
@@ -35,44 +20,6 @@ public class BadgeDb
             _db.Remove(assignment);
             await _db.SaveChangesAsync();
         }
-    }
-
-    public async Task<AssignedBadge?> GetAssignedBadge(int badgeId)
-    {
-        return await _db.AssignedBadges
-            .Include(x => x.Badge)
-            .Include(x => x.FromUser)
-            .Include(x => x.User)
-            .SingleOrDefaultAsync(x => x.Id == badgeId);
-    }
-
-    public async Task<List<AssignedBadge>> GetAssignmentHistory(Badge badge)
-    {
-        return await _db.AssignedBadges
-            .Where(x => x.Badge == badge)
-            .Include(x => x.User)
-            .Include(x => x.FromUser)
-            .ToListAsync();
-    }
-
-    public async Task SaveBadge(Badge badge, AssignedBadge initialAssignment)
-    {
-        _db.Badges.Add(badge);
-        _db.AssignedBadges.Add(initialAssignment);
-        await _db.SaveChangesAsync();
-    }
-
-    public async Task<List<BadgeType>> GetAllBadgeTypes() => await _db.BadgeTypes.ToListAsync();
-
-    public async Task<BadgeType?> GetBadgeType(int badgeId)
-    {
-        return await _db.BadgeTypes.SingleOrDefaultAsync(x => x.Id == badgeId);
-    }
-
-    public async Task UpdateBadge(Badge badge)
-    {
-        _db.Attach(badge).State = EntityState.Modified;
-        await _db.SaveChangesAsync();
     }
 
     public void DeleteBadge(Badge badge)
@@ -87,6 +34,55 @@ public class BadgeDb
         _db.SaveChanges();
     }
 
+    public async Task<List<Badge>> GetAllBadges()
+    {
+        return await _db.Badges.Include(x => x.BadgeType).ToListAsync();
+    }
+
+    public async Task<List<BadgeType>> GetAllBadgeTypes() => await _db.BadgeTypes.ToListAsync();
+
+    public async Task<AssignedBadge?> GetAssignedBadge(int badgeId)
+    {
+        return await _db.AssignedBadges
+            .Include(x => x.Badge)
+            .Include(x => x.FromUser)
+            .Include(x => x.User)
+            .SingleOrDefaultAsync(x => x.Id == badgeId);
+    }
+
+    public async Task<List<AssignedBadge>> GetAssignedBadges(User user)
+    {
+        return await _db.AssignedBadges.Include(x => x.Badge).Where(x => x.User == user).ToListAsync();
+    }
+
+    public async Task<List<AssignedBadge>> GetAssignedBadges(Guid userId)
+    {
+        return await _db.AssignedBadges.Include(x => x.Badge).Where(x => x.User.PrincipalId == userId).ToListAsync();
+    }
+
+    public async Task<List<AssignedBadge>> GetAssignmentHistory(Badge badge)
+    {
+        return await _db.AssignedBadges
+            .Where(x => x.Badge == badge)
+            .Include(x => x.User)
+            .Include(x => x.FromUser)
+            .ToListAsync();
+    }
+
+    public async Task<Badge?> GetBadge(int badgeId)
+    {
+        return await _db.Badges.Include(x => x.BadgeType).SingleOrDefaultAsync(m => m.Id == badgeId);
+    }
+
+    public async Task<int> GetBadgeAssignmentCount() => await _db.AssignedBadges.CountAsync();
+
+    public async Task<int> GetBadgeCount() => await _db.Badges.CountAsync();
+
+    public async Task<BadgeType?> GetBadgeType(int badgeId)
+    {
+        return await _db.BadgeTypes.SingleOrDefaultAsync(x => x.Id == badgeId);
+    }
+
     public async Task<List<AssignedBadge>> GetFullBadgeStream(int limit)
     {
         return await _db.AssignedBadges
@@ -96,33 +92,6 @@ public class BadgeDb
             .Include(x => x.FromUser)
             .Include(x => x.Badge)
             .ToListAsync();
-    }
-
-    public async Task<List<Badge>> GetAllBadges()
-    {
-        return await _db.Badges.Include(x => x.BadgeType).ToListAsync();
-    }
-
-    public async Task<int> GetBadgeCount() => await _db.Badges.CountAsync();
-
-    public async Task<int> GetBadgeAssignmentCount() => await _db.AssignedBadges.CountAsync();
-
-    public IEnumerable<(int place, User? User, int BadgeCount)> GetTopBadgeHolders()
-    {
-        var query =
-            _db.AssignedBadges
-                .Include(x => x.User)
-                .GroupBy(x => x.UserId)
-                .Select(group => new
-                {
-                    User = group.First().User, BadgeCount = group.Count()
-                })
-                .ToList()
-                .OrderByDescending(x => x.BadgeCount)
-                .Take(10);
-
-        //zip adds in the place number
-        return query.Zip(Enumerable.Range(1, 10000), (o, i) => (i, o.User, o.BadgeCount));
     }
 
     public IEnumerable<(int place, User? User, int BadgeCount)> GetTopBadgeGivers()
@@ -135,11 +104,42 @@ public class BadgeDb
                 {
                     User = group.First().FromUser, BadgeCount = group.Count()
                 })
-                .ToList()
                 .OrderByDescending(x => x.BadgeCount)
-                .Take(10);
+                .Take(10)
+                .ToList();
 
         //zip adds in the place number
         return query.Zip(Enumerable.Range(1, 10000), (o, i) => (i, o.User, o.BadgeCount));
+    }
+
+    public IEnumerable<(int place, User? User, int BadgeCount)> GetTopBadgeHolders()
+    {
+        var query =
+            _db.AssignedBadges
+                .Include(x => x.User)
+                .GroupBy(x => x.UserId)
+                .Select(group => new
+                {
+                    group.First().User, BadgeCount = group.Count()
+                })
+                .OrderByDescending(x => x.BadgeCount)
+                .Take(10)
+                .ToList();
+
+        //zip adds in the place number
+        return query.Zip(Enumerable.Range(1, 10000), (o, i) => (i, o.User, o.BadgeCount));
+    }
+
+    public async Task SaveBadge(Badge badge, AssignedBadge initialAssignment)
+    {
+        _db.Badges.Add(badge);
+        _db.AssignedBadges.Add(initialAssignment);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task UpdateBadge(Badge badge)
+    {
+        _db.Attach(badge).State = EntityState.Modified;
+        await _db.SaveChangesAsync();
     }
 }
